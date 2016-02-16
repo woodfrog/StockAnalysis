02ca14c35fe8f7e262589aafde3271ebbd9da835
CJC <cjc0722hz@gmail.com>
2016-02-16 21:41:20 +0800

    - update on 2-16, modify the content of volume strategy. --Version3.0

--- strategy_volume.m
+++ strategy_volume.m
@@ -5,19 +5,21 @@ function shift = strategy_volume ( dayIndex, volIndex, VOL_AVR, historyClose)
 parameter;
 
 if historyClose(dayIndex) >=  historyClose(dayIndex-1) %当天收盘价较前一天上涨
-     
+    
     volBuyFlag = 1;
     
     if volIndex <= 4 + VOL_BUY_DAY -1
         volBuyFlag = 0;
     else
-        if VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-1) && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-2) && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-3)...
-                && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-4)
+        if VOL_AVR(volIndex) >= VOL_JUDGE_UP_1 * VOL_AVR(volIndex-1) ...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_UP_1 * VOL_AVR(volIndex-2)...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_UP_1 * VOL_AVR(volIndex-3)...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_UP_1 * VOL_AVR(volIndex-4)
             volBuyFlag = 1;
         else
             for i = (volIndex - VOL_BUY_DAY + 1) : volIndex
-                if VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-1) && VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-2)...
-                        || VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-3) && VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-4)
+                if VOL_AVR(i) > VOL_JUDGE_UP_2 * VOL_AVR(i-1) && VOL_AVR(i) > VOL_JUDGE_UP_2 * VOL_AVR(i-2)...
+                        || VOL_AVR(i) > VOL_JUDGE_UP_2 * VOL_AVR(i-3) && VOL_AVR(i) > VOL_JUDGE_UP_2 * VOL_AVR(i-4)
                     %只有当连续VOL_BUY_DAY个x满足以上条件时，策略根据成交量变化选择买入
                 else
                     volBuyFlag = 0;
@@ -34,26 +36,28 @@ else  %当天收盘价较前一天下跌
     if volIndex <= 4 + VOL_SELL_DAY -1
         volSellFlag = 0;
     else
-        if VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-1) && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-2) && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-3)...
-                && VOL_AVR(volIndex) >= 2 * VOL_AVR(volIndex-4)
+        if VOL_AVR(volIndex) >= VOL_JUDGE_DOWN_1 * VOL_AVR(volIndex-1)...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_DOWN_1 * VOL_AVR(volIndex-2)...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_DOWN_1 * VOL_AVR(volIndex-3)...
+                && VOL_AVR(volIndex) >= VOL_JUDGE_DOWN_1 * VOL_AVR(volIndex-4)
             volSellFlag = 1;
         else
             for i = (volIndex - VOL_SELL_DAY + 1) : volIndex
-                if VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-1) && VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-2)...
-                        || VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-3) && VOL_AVR(i) > VOL_JUDGE * VOL_AVR(i-4)
+                if VOL_AVR(i) > VOL_JUDGE_DOWN_2 * VOL_AVR(i-1) && VOL_AVR(i) > VOL_JUDGE_DOWN_2 * VOL_AVR(i-2)...
+                        || VOL_AVR(i) > VOL_JUDGE_DOWN_2 * VOL_AVR(i-3) && VOL_AVR(i) > VOL_JUDGE_DOWN_2 * VOL_AVR(i-4)
                     %只有当连续VOL_BUY_DAY个x满足以上条件时，策略根据成交量变化选择买入
                 else
                     volSellFlag = 0;
                 end
             end
         end
-    end    
+    end
     
     if volSellFlag == 1
         shift = -1;
     else
         shift = 0;
-    end 
+    end
 end
 
 end
